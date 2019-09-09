40f0176288ab36dc261c3325482a298723d92111
Timothy Sharkey <Techne3@gmail.com>
2019-09-09 15:38:14 -0600

    app scoreboard

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React , {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const [down, setDown] = useState(4)
+
+  if(down === 0){
+    setDown(4)
+  }
+
+
   return (
+
+  
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        <button className="homeButtons__touchdown"onClick = {()=> setDown(down - 1)}>Down</button>
+     
+       
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -20,7 +32,12 @@ const BottomRow = () => {
         <h3 className="quarter__title">Quarter</h3>
         <div className="quarter__value">4</div>
       </div>
+   
+     
+    
+     
     </div>
+     
   );
 };
 
